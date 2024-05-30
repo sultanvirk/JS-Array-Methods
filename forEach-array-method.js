7ee840e6bbf984984b929a3534a6ac917a054bcc
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-30 15:10:37 +0500

    difference between forEach,map and filter

--- forEach-array-method.js
+++ forEach-array-method.js
@@ -1,6 +1,8 @@
 const numbers = [1, 2, 3, 3, 4, 1, 3]
 
 // forEach Array method take a callback function as a parameter & the call back function takes 3 parameters value, index, array
+// forEach can not return any thing it just iterate main array
+// While map and filter methods return a new array after performing operation on Main array.
 
 numbers.forEach((value, index, array) => {
     console.log('a[' + index + '] = ' + value)
