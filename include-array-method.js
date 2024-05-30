d3fe491ece3c95d1678ab06d63a4a322a0e509b3
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-30 17:14:07 +0500

    includes array method

---
+++ include-array-method.js
@@ -0,0 +1,8 @@
+const names = ["xyz", "abc", "qwerty"]
+
+// includes method is used to find if the given value is present in the array or not
+// Example:-
+
+let res = names.includes("abc")
+
+console.log(res)
