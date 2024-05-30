ceba9498717fee4195a10f4cb23b250eb0f80914
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-30 17:19:08 +0500

    join array method

---
+++ join-array-method.js
@@ -0,0 +1,11 @@
+const countries = ['Russia', 'USA', 'Germany', 'Australia', 'Canada']
+
+// Join method return the values with given separator
+// Example :-  array.join(separator)
+// array.join(' - ')
+// array.join(' , ')
+// array.join('')
+
+const res = countries.join(' - ')
+
+console.log(res)
