c6d83c5f3e21c2feccfafa03cd112ac837577ecb
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-30 15:01:16 +0500

    forEach JS method with an exercise

---
+++ forEach-array-method.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 3, 4, 1, 3]
+
+// forEach Array method take a callback function as a parameter & the call back function takes 3 parameters value, index, array
+
+numbers.forEach((value, index, array) => {
+    console.log('a[' + index + '] = ' + value)
+})
+
+// Problem
+// We need to calculate the count of items in array.
+
+const count = {}
+
+numbers.forEach((value) => {
+    if(count[value]){
+        count[value]++;
+    }else{
+        count[value]=1;
+    }
+})
+
+console.log(count)
