f76884793a2101d6d632cc25867d6f76c7cd74e6
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-30 17:10:43 +0500

    fill method

---
+++ fill-array-method.js
@@ -0,0 +1,12 @@
+const numbers = [1,2,3,4,5]
+
+
+// fill method can take 3 parameters which are
+// 1 _ value to insert
+// 2 _ start index
+// 3 _ last end index
+// Example :- array.fill(value, startIndex, endIndex)
+
+// numbers.fill(0)
+numbers.fill('*',2,4)
+console.log(numbers)
