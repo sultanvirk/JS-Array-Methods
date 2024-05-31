c8be76076c42c036007c2e247923c1932029a16d
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 15:18:15 +0500

    unshift array method

---
+++ unshift-array-method.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3]
+
+
+// unshift method is used ton add new elements in the biggning of the array and returns the new length of the array
+
+const newLength = numbers.unshift(0,1,2,-4)
+
+console.log(numbers)
+console.log(newLength)
