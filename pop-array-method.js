f072a17382852f9bb0b669e5b63f61199ccc3f76
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 15:13:58 +0500

    pop array method

---
+++ pop-array-method.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6]
+
+//  The pop method removes the last item from an array
+//  It returns the last item if we want then assign a variable
+
+const lastItem = numbers.pop()
+
+console.log(lastItem)
+console.log(numbers)
