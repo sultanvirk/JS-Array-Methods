5e3cd21c1cd6cae70b834869035e704a3e1d4322
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 15:22:32 +0500

    updates

---
+++ shift-array-method.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5]
+
+// The shift method in array removes the element from biggning of array and returns it
+// we can say that its the opposite of pop method
+
+const removedElement = numbers.shift()
+
+console.log(numbers)
+console.log(removedElement)
--- unshift-array-method.js
+++ unshift-array-method.js
@@ -1,8 +1,8 @@
 const numbers = [1,2,3]
 
 
-// unshift method is used ton add new elements in the biggning of the array and returns the new length of the array
-
+// unshift method is used to add new elements in the biggning of the array and returns the new length of the array
+//  It is the opposite of push method
 const newLength = numbers.unshift(0,1,2,-4)
 
 console.log(numbers)
