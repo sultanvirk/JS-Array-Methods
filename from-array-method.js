85c409dabe6e55d821290f27abba4fd6c1da9545
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 16:00:24 +0500

    from array method

---
+++ from-array-method.js
@@ -0,0 +1,9 @@
+const str = '1234567'
+
+//  from array method is used to converet string to array
+//  It takes two parameters
+// Example:- Array.from(string, dataType)
+
+const res = Array.from(str , Number)
+
+console.log(res)
