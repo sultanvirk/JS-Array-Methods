7140e2cc8ec92b31b3f58e9527d19499ada5b3d6
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 16:05:59 +0500

    isArray array method

---
+++ isArray-array-method.js
@@ -0,0 +1,10 @@
+const array = [1,2,3,4,5]
+const str = 'hello world';
+const num = 23;
+
+// isArray method is used to check the given argument is array or not
+// It returns bolean values
+
+console.log(Array.isArray(array))
+console.log(Array.isArray(str))
+console.log(Array.isArray(num))
