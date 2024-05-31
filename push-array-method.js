280023f29ad8afc45ca58f2a3639d39e85dddb60
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 15:10:29 +0500

    push array method

---
+++ push-array-method.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4,5]
+
+//  push method is used to add elements to array but it added elements in the end of array
+// Example :-
+// numbers.push(19)
+// Push method returns the total length of the new array
+// so, we assign a variable
+const total = numbers.push(7,5,4,6)
+console.log(total)
+console.log(numbers)
